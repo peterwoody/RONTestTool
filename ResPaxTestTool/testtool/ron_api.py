f0e8e42bcead207dec9f5888254268d8b3101c7f
Stiglits <shaquille.powiesnik@my.jcu.edu.au>
2015-12-21 13:48:53 +1000

    Second level data complete
    -displays all required information in required format
    -next step is to implement multithreading to increase overall
    performance

--- ResPaxTestTool/testtool/ron_api.py
+++ ResPaxTestTool/testtool/ron_api.py
@@ -1,21 +1,21 @@
 import xmlrpclib
 
-connection = False
+connection = xmlrpclib.Server('https://ron.respax.com.au:30443/section/xmlrpc/server-ron.php?config=train')
 
 
 def logout():
     global connection
-    connection = False
+    connection = xmlrpclib.Server('https://ron.respax.com.au:30443/section/xmlrpc/server-ron.php?config=train')
 
 
 def get_connection(username, password):
     url = 'https://ron.respax.com.au:30443/section/xmlrpc/server-ron.php?config=train'
-    ron = xmlrpclib.Server(url, allow_none=True)
+    ron = xmlrpclib.ServerProxy(url, allow_none=True)
 
     try:
         session_id = ron.login(username, password)
         global connection
-        connection = xmlrpclib.Server(url + '&' + session_id)
+        connection = xmlrpclib.ServerProxy(url + '&' + session_id)
         return {'logic': True}
     except xmlrpclib.Fault as err:
         return {'logic': False, 'fault': "A fault occurred. Fault code: %d." % err.faultCode +
@@ -36,19 +36,19 @@ def get_hosts(key):
     return list_object
 
 
-def get_host_details(key):
+def get_host_details():
     host_ids = get_hosts('strHostID')
     #list_object = [connection.readHostDetails(data) for data in host_ids]
-    list_object = []
+    #list_object = []
 
-    for data in host_ids:
-        host_details = connection.readHostDetails(data)[0].get(key)
-        # host_detail = host_details[0].get(key, None)
-        print(host_details)
-        list_object.append(host_details)
-
-    print(list_object)
-    return list_object
+    # for data in host_ids:
+    #     host_details = connection.readHostDetails(data)[0].get(key)
+    #     # host_detail = host_details[0].get(key, None)
+    #     print(host_details)
+    #     list_object.append(host_details)
+    #
+    # print(list_object)
+    # return list_object
 
 
 def read_tours(host_id):
@@ -60,10 +60,16 @@ def read_tours(host_id):
 def read_tour_bases(host_id, tour_code):
     tour_bases = connection.readTourBases(host_id, tour_code)
     return tour_bases
-#read_tours()
 
-#readTourBases (string hostid, string tourcode)
-#host_details = get_host_details()
-#get_host_details('strLocation')
-#host_details = get_connection().readHostDetails(host_id)
-#return host_details
+
+def read_tour_times(host_id, tour_code):
+    tour_times = connection.readTourTimes(host_id, tour_code)
+    return tour_times
+
+
+def read_tour_pickups(host_id, tour_code, tour_time_id, basis_id):
+    tour_pickups = connection.readTourPickups(host_id, tour_code, tour_time_id, basis_id)
+
+    return tour_pickups
+
+
--- ResPaxTestTool/testtool/views.py
+++ ResPaxTestTool/testtool/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from models import AddOperator
-from django.contrib import auth
 from django.http import JsonResponse
 import ron_api
 
@@ -15,54 +13,16 @@ def testtool(request):
     connection = ron_api.get_connection(username, password)
 
     if connection.get('logic'):
-        # host_name = AddOperator.objects.values_list('operator', flat=True)
-        # host_info = ron_api.get_hosts()
-
-        locations = AddOperator.objects.values_list('location', flat=True)
-        basis = AddOperator.objects.values_list('basis', flat=True)
-        subbasis = AddOperator.objects.values_list('subbasis', flat=True)
-        time = AddOperator.objects.values_list('time', flat=True)
-        pickup_id = AddOperator.objects.values_list('pickupId', flat=True)
-
-        # print(host_info)
-
-        def convert_to_list(value):
-            list_object = []
-            for data in value:
-                # print(data)
-                host = data
-                # print(str(host))
-                list_object.append(str(host))
-
-            list_object.sort()
-            # print(list_object)
-            return list_object
-
-        # host_name = convert_to_list(host_info, 'strHostName')
-        # locations = convert_to_list(locations)
 
         host_name = ron_api.get_hosts('strHostName')
         host_id = ron_api.get_hosts('strHostID')
 
-        # host_details = ron_api.host_details
-        # print(host_details[0].get('strLocation'))
-
         #locations = ron_api.get_host_details('strLocation')
-        basis = convert_to_list(basis)
-        subbasis = convert_to_list(subbasis)
-        time = convert_to_list(time)
-        pickup_id = convert_to_list(pickup_id)
 
-        print(host_name)
-        print(host_id)
         context = {
             "host_name": host_name,
-            "locations": locations,
+            #"locations": locations,
             "host_id": host_id,
-            "basis": basis,
-            "subbasis": subbasis,
-            "time": time,
-            "pickup_id": pickup_id,
         }
         return render(request, "testTool.html", context)
     else:
@@ -98,21 +58,29 @@ def get_tours(request):
     host_id = request.POST['id']
     tours = ron_api.read_tours(host_id)
     tour_bases = []
+    tour_times = []
+    tour_pickups = []
     for data in tours:
         tour_code = data.get('strTourCode')
-        print(tour_code)
-        tour_base = ron_api.read_tour_bases(host_id, tour_code)
-        print(tour_base)
-        tour_bases.append(tour_base)
 
-    print(tours)
+        tour_base = ron_api.read_tour_bases(host_id, tour_code)
+        tour_time = ron_api.read_tour_times(host_id, tour_code)
+
+        if tour_base:
+            tour_pickup = ron_api.read_tour_pickups(host_id, tour_code, tour_time[0].get('intTourTimeID'),
+                                                     tour_base[0].get('intBasisID'))
+        else:
+            tour_pickup = ''
+
+        tour_bases.append(tour_base)
+        tour_times += tour_time
+        tour_pickups += tour_pickup
+
     response_data = {
         'tours': tours,
         'tour_bases': tour_bases,
-        #'subbasis': tours,
-        'pickup_time': tours,
-        'pickup_id': tours,
+        'tour_times': tour_times,
+        'tour_pickups': tour_pickups,
     }
-    print(response_data)
 
     return JsonResponse(response_data)

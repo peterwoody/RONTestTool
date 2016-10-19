df14b3f5b09fbc6a8b85198ea9ce080d63d8a0ae
Stiglits <shaquille.powiesnik@my.jcu.edu.au>
2016-10-19 17:23:21 +1000

    Fixed Duplicating Elements in Table
    -a problem to do with ordering items in the table A-Z caused duplicate
    elements in the main table.
    
    -fixed by checking if the index value had already been used.

--- ResPaxTestTool/static/assets/js/table_creation.js
+++ ResPaxTestTool/static/assets/js/table_creation.js
@@ -107,17 +107,21 @@ function get_tours(tableRow, id, server_url) {
                     var tour_codes = json.tours;
                     var keys = get_sorted_keys(tour_codes, "strTourCode");
 
+                    var j = [];
                     for (var i = 0; i in tour_codes; i++) {
-                        var j = 0;
-                        while (tour_codes[j]['strTourCode'] !== keys[i]) {
-                            j++
+                        var k = 0;
+                        while (tour_codes[k]['strTourCode'] !== keys[i] || j.includes(k) === true) {
+                            k++;
                         }
+                        j.push(k);
 
-                        var new_id = id + ',' + tour_codes[j]['strTourName'].toString() + ',' + tour_codes[j]['strTourCode'].toString();
+                        var tour_name = tour_codes[k]['strTourName'].toString();
+                        var tour_code = tour_codes[k]['strTourCode'].toString();
+
+                        var new_id = id + ',' + tour_name + ',' + tour_code;
                         var newTableRow = document.createElement('tr');
                         var tour_code_td = document.createElement('td');
-                        var tour_code = json.tours[j]['strTourCode'];
-                        var tour_name = json.tours[j]['strTourName'];
+
                         var tour_code_td_value = 'Tour Code: ' + tour_code + '<br> Tour Name: ' + tour_name;
                         var string_p = document.createElement("p");
                         var string_value = document.createTextNode("(" +
@@ -193,33 +197,37 @@ function get_tour_bases(tableRow, id, server_url) {
         success: function (json) {
             loading_img.remove();
             if (!jQuery.isEmptyObject(json.tour_bases)) {
+                var j = [];
                 for (var i = 0; i < json.tour_bases.length; i++) {
                     var tour_bases = json.tour_bases;
 
                     var keys = get_sorted_keys(tour_bases, "strBasisDesc");
-                    var j = 0;
-                    while (tour_bases[j]['strBasisDesc'] !== keys[i]) {
-                        j++
+                    var k = 0;
+
+                    while (tour_bases[k]['strBasisDesc'] !== keys[i] || j.includes(k) === true) {
+                        k++;
                     }
-                    var tour_basis_name = tour_bases[j]['strBasisDesc'];
-                    var tour_sub_basis_name = tour_bases[j]['strSubBasisDesc'];
-                    var tour_basis_id = json.tour_bases[j]['intBasisID'];
-                    var tour_sub_basis_id = json.tour_bases[j]['intSubBasisID'];
+                    j.push(k);
+                    var tour_basis_name = tour_bases[k]['strBasisDesc'];
+                    var tour_sub_basis_name = tour_bases[k]['strSubBasisDesc'];
+                    var tour_basis_id = json.tour_bases[k]['intBasisID'];
+                    var tour_sub_basis_id = json.tour_bases[k]['intSubBasisID'];
+
                     var new_id = id + ',' + tour_basis_name + ',' + tour_basis_id + ',' + tour_sub_basis_name + ',' + tour_sub_basis_id;
                     var newTableRow = document.createElement('tr');
 
                     var basis_td = document.createElement("td");
                     var subbasis_td = document.createElement("td");
 
-                    var basis_td_value = document.createTextNode("Basis: " + tour_basis_name + ' (' + json.tour_bases[j]['intBasisID'] + ')');
+                    var basis_td_value = document.createTextNode("Basis: " + tour_basis_name + ' (' + tour_basis_id + ')');
 
-                    var subbasis_td_value = document.createTextNode("Subbasis: " + tour_sub_basis_name + ' (' + json.tour_bases[j]['intSubBasisID'] + ')');
+                    var subbasis_td_value = document.createTextNode("Subbasis: " + tour_sub_basis_name + ' (' + tour_sub_basis_id + ')');
                     var string_p = document.createElement("p");
                     var string_value = document.createTextNode("(" +
                         host_id + "," +
                         tour_code + "," +
-                        json.tour_bases[j]['intBasisID'] + "," +
-                        json.tour_bases[j]['intSubBasisID'] +
+                        tour_basis_id + "," +
+                        tour_sub_basis_id +
                         ')');
 
                     string_p.setAttribute("style", "float: right;");
@@ -295,15 +303,28 @@ function get_tour_times(tableRow, id, server_url) {
         success: function (json) {
             loading_img.remove();
             if (!jQuery.isEmptyObject(json.tour_times)) {
+                var j = [];
                 for (var i = 0; i < json.tour_times.length; i++) {
-                    var tour_time_id = json.tour_times[i]['intTourTimeID'];
-                    var new_id = id + ',' + json.tour_times[i]['intTourTimeID'];
+                    var tour_times = json.tour_times;
+
+                    var keys = get_sorted_keys(tour_times, "dteTourTime");
+
+                    var k = 0;
+                    while (tour_times[k]['dteTourTime'] !== keys[i] || j.includes(k) === true) {
+                        k++;
+                    }
+                    j.push(k);
+
+                    var tour_time_id = tour_times[k]['intTourTimeID'];
+                    var tour_time = tour_times[k]['dteTourTime']['iso8601'];
+
+                    var new_id = id + ',' + tour_time_id;
 
                     var newTableRow = document.createElement('tr');
 
                     var time_td = document.createElement("td");
 
-                    var time_td_value = document.createTextNode("Time: " + json.tour_times[i]['dteTourTime']['iso8601'] + ' (' + tour_time_id + ')');
+                    var time_td_value = document.createTextNode("Time: " + tour_time + ' (' + tour_time_id + ')');
                     var string_p = document.createElement("p");
                     var string_value = document.createTextNode("(" +
                         host_id + "," +
@@ -385,20 +406,29 @@ function get_tour_pickups(tableRow, id, server_url) {
 
         success: function (json) {
             loading_img.remove();
+            var j = [];
             for (var i = 0; i < json.tour_pickups.length; i++) {
                 var tour_pickups = json.tour_pickups;
                 var keys = get_sorted_keys(tour_pickups, "strPickupValue");
-                var j = 0;
-                while (tour_pickups[j]['strPickupValue'] !== keys[i]) {
-                    j++
+                var k = 0;
+                while (tour_pickups[k]['strPickupValue'] !== keys[i] || j.includes(k) === true) {
+                    k++;
                 }
-                var new_id = id + ',' + json.tour_pickups[j]['strPickupKey'];
+                j.push(k);
+
+                var tour_pickup_value = tour_pickups[k]['strPickupValue'];
+                var tour_pickup_key = tour_pickups[k]['strPickupKey'];
+
+
+                console.log(j);
+
+                var new_id = id + ',' + tour_pickup_key;
                 var newTableRow = document.createElement('tr');
 
                 var pickup_id_td = document.createElement("td");
                 var string_td = document.createElement("td");
 
-                var pickup_id_td_value = document.createTextNode("Pickup ID: " + json.tour_pickups[j]['strPickupValue'] + ' ' + '(' + json.tour_pickups[j]['strPickupKey'] + ')');
+                var pickup_id_td_value = document.createTextNode("Pickup ID: " + tour_pickup_value + ' ' + '(' + tour_pickup_key + ')');
 
                 var string_td_value = document.createTextNode("String: (" +
                     host_id + ',' +
@@ -406,7 +436,7 @@ function get_tour_pickups(tableRow, id, server_url) {
                     tour_basis_id + ',' +
                     tour_sub_basis_id + ',' +
                     tour_time_id + ',' +
-                    json.tour_pickups[j]['strPickupKey'] +
+                    tour_pickup_key +
                     ')');
 
                 pickup_id_td.appendChild(pickup_id_td_value);
@@ -418,7 +448,7 @@ function get_tour_pickups(tableRow, id, server_url) {
                     tour_basis_id + "','" +
                     tour_sub_basis_id + "','" +
                     tour_time_id + "','" +
-                    json.tour_pickups[i]['strPickupKey'] + "')");
+                    tour_pickup_key + "')");
                 pickup_id_td.setAttribute('colspan', '1');
                 string_td.setAttribute('colspan', '3');
                 newTableRow.setAttribute("data-level", "5");
